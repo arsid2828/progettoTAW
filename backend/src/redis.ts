8e752d3a070e50a91840fb42ff1d27cf79e41d16
RicuxUni <902855@stud.unive.it>
2025-12-06 16:29:09 +0100

    cambiato  docker compose, pregodio

--- backend/src/redis.ts
+++ backend/src/redis.ts
@@ -1,5 +1,6 @@
 import { Redis } from 'ioredis';
 export const redis = new Redis({
-  host: 'localhost',        // nome del container, non localhost!
-  port: 6379,
+  host: 'redis',
+  port:  6379,
+  retryStrategy: times => Math.min(times * 50, 2000),
 });
